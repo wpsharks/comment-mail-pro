176a39a0b4eb3f664827d2807dd5e9f3c487a6d5
Raam Dev <raam@websharks-inc.com>
2015-12-11 10:17:53 -0500

    Remove references to Comment ID#; confusing and irrelevant for users
    See websharks/comment-mail#77

--- comment-mail-pro/includes/templates/type-a/email/comment-notification/message.php
+++ comment-mail-pro/includes/templates/type-a/email/comment-notification/message.php
@@ -69,7 +69,7 @@ $replies_via_email_enable = $sub_post_comments_open && $plugin->options['replies
 				<?php if($subscribed_to_own_comment): ?>
 					<?php echo sprintf(__('New Replies to <a href="%1$s">your Comment</a> on "%2$s"', $plugin->text_domain), esc_attr($sub_comment_url), esc_html($sub_post_title_clip)); ?>
 				<?php else: // The comment was not authored by this subscriber; i.e. it's not their own. ?>
-					<?php echo sprintf(__('New Replies to <a href="%1$s">Comment ID #%2$s</a> on "%3$s"', $plugin->text_domain), esc_attr($sub_comment_url), esc_html($sub_comment->comment_ID), esc_html($sub_post_title_clip)); ?>
+					<?php echo sprintf(__('New Replies to <a href="%1$s">a Comment</a> on "%2$s"', $plugin->text_domain), esc_attr($sub_comment_url), esc_html($sub_post_title_clip)); ?>
 				<?php endif; ?>
 
 			<?php else: // All comments/replies on this post. ?>
@@ -83,7 +83,7 @@ $replies_via_email_enable = $sub_post_comments_open && $plugin->options['replies
 				<?php if($subscribed_to_own_comment): ?>
 					<?php echo sprintf(__('New Reply to <a href="%1$s">your Comment</a> on "%2$s"', $plugin->text_domain), esc_attr($sub_comment_url), esc_html($sub_post_title_clip)); ?>
 				<?php else: // The comment was not authored by this subscriber; i.e. it's not their own. ?>
-					<?php echo sprintf(__('New Reply to <a href="%1$s">Comment ID #%2$s</a> on "%3$s"', $plugin->text_domain), esc_attr($sub_comment_url), esc_html($sub_comment->comment_ID), esc_html($sub_post_title_clip)); ?>
+					<?php echo sprintf(__('New Reply to <a href="%1$s">a Comment</a> on "%2$s"', $plugin->text_domain), esc_attr($sub_comment_url), esc_html($sub_post_title_clip)); ?>
 				<?php endif; ?>
 
 			<?php else: // All comments/replies on this post ID. ?>
