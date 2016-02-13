0a3d503be2f3c7772f5f662f00efd08ff4715de7
jaswsinc <jaswsinc@wsharks.com>
2016-02-12 17:58:32 -0900

    Adding `GLOBAL_NS` const.

--- src/includes/stub.php
+++ src/includes/stub.php
@@ -10,3 +10,5 @@ if (!defined('WPINC')) {
     exit('Do NOT access this file directly: '.basename(__FILE__));
 }
 require_once dirname(dirname(__FILE__)).'/vendor/autoload.php';
+
+define(__NAMESPACE__.'\\GLOBAL_NS', 'comment_mail');
