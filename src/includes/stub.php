3e88644f43c4d6a9e560ac2847c647f14d81b271
jaswsinc <jaswsinc@wsharks.com>
2016-02-12 17:54:14 -0900

    Moving plugin includes used by stubs.

---
+++ src/includes/stub.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Stub.
+ *
+ * @since 160212 PSR compliance.
+ */
+namespace WebSharks\CommentMail\Pro;
+
+if (!defined('WPINC')) {
+    exit('Do NOT access this file directly: '.basename(__FILE__));
+}
+require_once dirname(dirname(__FILE__)).'/vendor/autoload.php';
