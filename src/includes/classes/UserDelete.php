d1028a8631c165d2279b474bd168fd43fb8f33d7
jaswsinc <jaswsinc@wsharks.com>
2016-02-14 01:26:18 -0900

    Refactor/format `UserDelete`

--- src/includes/classes/UserDelete.php
+++ src/includes/classes/UserDelete.php
@@ -2,82 +2,78 @@
 /**
  * User Deletion Handler
  *
- * @since 141111 First documented version.
+ * @since     141111 First documented version.
  * @copyright WebSharks, Inc. <http://www.websharks-inc.com>
- * @license GNU General Public License, version 3
+ * @license   GNU General Public License, version 3
  */
 namespace WebSharks\CommentMail\Pro;
 
+/**
+ * User Deletion Handler
+ *
+ * @since 141111 First documented version.
+ */
+class UserDelete extends AbsBase
+{
+    /**
+     * @var integer User ID.
+     *
+     * @since 141111 First documented version.
+     */
+    protected $user_id;
 
+    /**
+     * @var integer Blog ID.
+     *
+     * @since 141111 First documented version.
+     */
+    protected $blog_id;
 
-		/**
-		 * User Deletion Handler
-		 *
-		 * @since 141111 First documented version.
-		 */
-	class UserDelete extends AbsBase
-		{
-			/**
-			 * @var integer User ID.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected $user_id;
+    /**
+     * @var boolean Switched blog?
+     *
+     * @since 141111 First documented version.
+     */
+    protected $switched_blog;
 
-			/**
-			 * @var integer Blog ID.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected $blog_id;
+    /**
+     * Class constructor.
+     *
+     * @param integer|string $user_id User ID.
+     * @param integer|string $blog_id Blog ID. Defaults to `0` (current blog).
+     *
+     * @since 141111 First documented version.
+     */
+    public function __construct($user_id, $blog_id = 0)
+    {
+        parent::__construct();
 
-			/**
-			 * @var boolean Switched blog?
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected $switched_blog;
+        $this->switched_blog = false;
+        $this->user_id       = (integer)$user_id;
+        $this->blog_id       = (integer)$blog_id;
 
-			/**
-			 * Class constructor.
-			 *
-			 * @param integer|string $user_id User ID.
-			 * @param integer|string $blog_id Blog ID. Defaults to `0` (current blog).
-			 *
-			 * @since 141111 First documented version.
-			 */
-			public function __construct($user_id, $blog_id = 0)
-			{
-				parent::__construct();
+        $this->maybePurgeSubs();
+    }
 
-				$this->switched_blog = FALSE;
-				$this->user_id       = (integer)$user_id;
-				$this->blog_id       = (integer)$blog_id;
+    /**
+     * Purges subscriptions.
+     *
+     * @since 141111 First documented version.
+     */
+    protected function maybePurgeSubs()
+    {
+        if (!$this->user_id) {
+            return; // Nothing to do.
+        }
+        if ($this->blog_id && $this->blog_id !== $GLOBALS['blog_id']) {
+            switch_to_blog($this->blog_id);
+            $this->switched_blog = true;
+        }
+        new SubPurger(0, 0, $this->user_id);
 
-				$this->maybe_purge_subs();
-			}
-
-			/**
-			 * Purges subscriptions.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected function maybe_purge_subs()
-			{
-				if(!$this->user_id)
-					return; // Nothing to do.
-
-				if($this->blog_id && $this->blog_id !== $GLOBALS['blog_id'])
-				{
-					switch_to_blog($this->blog_id);
-					$this->switched_blog = TRUE;
-				}
-				new SubPurger(0, 0, $this->user_id);
-
-				if($this->blog_id && $this->switched_blog)
-				{
-					restore_current_blog();
-					$this->switched_blog = FALSE;
-				}
-			}
-		}
+        if ($this->blog_id && $this->switched_blog) {
+            restore_current_blog();
+            $this->switched_blog = false;
+        }
+    }
+}
