a6e4cdc3e3ae3153cc13f5288af4cd964660e924
jaswsinc <jaswsinc@wsharks.com>
2016-02-13 19:25:49 -0900

    Reformat test.

--- src/includes/classes/ApiBase.php
+++ src/includes/classes/ApiBase.php
@@ -2,14 +2,14 @@
 /**
  * API abstraction.
  *
- * @since 160212 PSR compliance.
+ * @since     160212 PSR compliance.
  *
  * @copyright WebSharks, Inc. <http://www.websharks-inc.com>
- * @license GNU General Public License, version 3
+ * @license   GNU General Public License, version 3
  */
 namespace WebSharks\CommentMail\Pro;
 
-abstract class ApiBase extends AbsBase
+abstract class ApiBase
 {
     public static function subOps()
     {
--- src/includes/classes/actions.php
+++ src/includes/classes/actions.php
@@ -2,104 +2,103 @@
 /**
  * Actions
  *
- * @since 141111 First documented version.
+ * @since     141111 First documented version.
  * @copyright WebSharks, Inc. <http://www.websharks-inc.com>
- * @license GNU General Public License, version 3
+ * @license   GNU General Public License, version 3
  */
 namespace WebSharks\CommentMail\Pro;
 
+/**
+ * Actions
+ *
+ * @since 141111 First documented version.
+ *
+ * @note (front|back)-end actions share the SAME namespace.
+ *    i.e. `$_REQUEST[GLOBAL_NS][action]`, where `action` should be unique
+ *    across any/all (front|back)-end action handlers.
+ *
+ *    This limitation applies only within each classification (context).
+ *    Front-end actions CAN have the same `[action]` name as a back-end action,
+ *    since they're already called from completely different contexts on-site.
+ */
+class Actions extends AbsBase
+{
+    /**
+     * Class constructor.
+     *
+     * @since 141111 First documented version.
+     */
+    public function __construct()
+    {
+        parent::__construct();
 
-		/**
-		 * Actions
-		 *
-		 * @since 141111 First documented version.
-		 *
-		 * @note (front|back)-end actions share the SAME namespace.
-		 *    i.e. `$_REQUEST[GLOBAL_NS][action]`, where `action` should be unique
-		 *    across any/all (front|back)-end action handlers.
-		 *
-		 *    This limitation applies only within each classification (context).
-		 *    Front-end actions CAN have the same `[action]` name as a back-end action,
-		 *    since they're already called from completely different contexts on-site.
-		 */
-	class Actions extends AbsBase
-		{
-			/**
-			 * Class constructor.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			public function __construct()
-			{
-				parent::__construct();
+        $this->maybeDoSsoActions();
+        $this->maybeDoSubActions();
+        $this->maybeDoWebhookActions();
+        $this->maybeDoMenuPageActions();
+    }
 
-				$this->maybeDoSsoActions();
-				$this->maybeDoSubActions();
-				$this->maybeDoWebhookActions();
-				$this->maybeDoMenuPageActions();
-			}
+    /**
+     * Single sign-on actions.
+     *
+     * @since 141111 First documented version.
+     */
+    protected function maybeDoSsoActions()
+    {
+        if (is_admin()) {
+            return; // Not applicable.
+        }
+        if (empty($_REQUEST[GLOBAL_NS])) {
+            return; // Nothing to do.
+        }
+        new SsoActions();
+    }
 
-			/**
-			 * Single sign-on actions.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected function maybeDoSsoActions()
-			{
-				if(is_admin())
-					return; // Not applicable.
+    /**
+     * Subscriber actions.
+     *
+     * @since 141111 First documented version.
+     */
+    protected function maybeDoSubActions()
+    {
+        if (is_admin()) {
+            return; // Not applicable.
+        }
+        if (empty($_REQUEST[GLOBAL_NS])) {
+            return; // Nothing to do.
+        }
+        new SubActions();
+    }
 
-				if(empty($_REQUEST[GLOBAL_NS]))
-					return; // Nothing to do.
+    /**
+     * Webhook actions.
+     *
+     * @since 141111 First documented version.
+     */
+    protected function maybeDoWebhookActions()
+    {
+        if (is_admin()) {
+            return; // Not applicable.
+        }
+        if (empty($_REQUEST[GLOBAL_NS])) {
+            return; // Nothing to do.
+        }
+        new WebhookActions();
+    }
 
-				new SsoActions();
-			}
-
-			/**
-			 * Subscriber actions.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected function maybeDoSubActions()
-			{
-				if(is_admin())
-					return; // Not applicable.
-
-				if(empty($_REQUEST[GLOBAL_NS]))
-					return; // Nothing to do.
-
-				new SubActions();
-			}
-
-			/**
-			 * Webhook actions.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected function maybeDoWebhookActions()
-			{
-				if(is_admin())
-					return; // Not applicable.
-
-				if(empty($_REQUEST[GLOBAL_NS]))
-					return; // Nothing to do.
-
-				new WebhookActions();
-			}
-
-			/**
-			 * Menu page actions.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected function maybeDoMenuPageActions()
-			{
-				if(!is_admin())
-					return; // Not applicable.
-
-				if(empty($_REQUEST[GLOBAL_NS]))
-					return; // Nothing to do.
-
-				new MenuPageActions();
-			}
-		}
+    /**
+     * Menu page actions.
+     *
+     * @since 141111 First documented version.
+     */
+    protected function maybeDoMenuPageActions()
+    {
+        if (!is_admin()) {
+            return; // Not applicable.
+        }
+        if (empty($_REQUEST[GLOBAL_NS])) {
+            return; // Nothing to do.
+        }
+        new MenuPageActions();
+    }
+}
