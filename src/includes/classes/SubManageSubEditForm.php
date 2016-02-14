1e66c08f3e60c5d5b1f13c72054c19b139af02cd
jaswsinc <jaswsinc@wsharks.com>
2016-02-14 01:11:52 -0900

    Refactor/format `SubManageSubEditForm`

--- src/includes/classes/SubManageSubEditForm.php
+++ src/includes/classes/SubManageSubEditForm.php
@@ -2,30 +2,28 @@
 /**
  * Sub. Management Sub. Edit Form
  *
- * @since 141111 First documented version.
+ * @since     141111 First documented version.
  * @copyright WebSharks, Inc. <http://www.websharks-inc.com>
- * @license GNU General Public License, version 3
+ * @license   GNU General Public License, version 3
  */
 namespace WebSharks\CommentMail\Pro;
 
-
-
-		/**
-		 * Sub. Management Sub. Edit Form
-		 *
-		 * @since 141111 First documented version.
-		 */
-	class SubManageSubEditForm extends SubManageSubFormBase
-		{
-			/**
-			 * Class constructor.
-			 *
-			 * @since 141111 First documented version.
-			 *
-			 * @param integer $sub_key Unique subscription key.
-			 */
-			public function __construct($sub_key)
-			{
-				parent::__construct((string)$sub_key);
-			}
-		}
+/**
+ * Sub. Management Sub. Edit Form
+ *
+ * @since 141111 First documented version.
+ */
+class SubManageSubEditForm extends SubManageSubFormBase
+{
+    /**
+     * Class constructor.
+     *
+     * @since 141111 First documented version.
+     *
+     * @param integer $sub_key Unique subscription key.
+     */
+    public function __construct($sub_key)
+    {
+        parent::__construct((string)$sub_key);
+    }
+}
