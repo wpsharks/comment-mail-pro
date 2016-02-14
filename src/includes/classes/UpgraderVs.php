4af4b28c1bebe3f806e9ee8a7ebb1e8125b1e558
jaswsinc <jaswsinc@wsharks.com>
2016-02-14 01:24:57 -0900

    Refactor/format `UpgraderVs`

--- src/includes/classes/UpgraderVs.php
+++ src/includes/classes/UpgraderVs.php
@@ -2,87 +2,88 @@
 /**
  * Upgrader (Version-Specific)
  *
- * @since 141111 First documented version.
+ * @since     141111 First documented version.
  * @copyright WebSharks, Inc. <http://www.websharks-inc.com>
- * @license GNU General Public License, version 3
+ * @license   GNU General Public License, version 3
  */
 namespace WebSharks\CommentMail\Pro;
 
+/**
+ * Upgrader (Version-Specific)
+ *
+ * @since 141111 First documented version.
+ */
+class UpgraderVs extends AbsBase
+{
+    /**
+     * @var string Previous version.
+     *
+     * @since 141111 First documented version.
+     */
+    protected $prev_version;
 
+    /**
+     * Class constructor.
+     *
+     * @since 141111 First documented version.
+     *
+     * @param string $prev_version Version they are upgrading from.
+     */
+    public function __construct($prev_version)
+    {
+        parent::__construct();
 
-		/**
-		 * Upgrader (Version-Specific)
-		 *
-		 * @since 141111 First documented version.
-		 */
-	class UpgraderVs extends AbsBase
-		{
-			/**
-			 * @var string Previous version.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected $prev_version;
+        $this->prev_version = (string)$prev_version;
 
-			/**
-			 * Class constructor.
-			 *
-			 * @since 141111 First documented version.
-			 *
-			 * @param string $prev_version Version they are upgrading from.
-			 */
-			public function __construct($prev_version)
-			{
-				parent::__construct();
+        $this->runHandlers(); // Run upgrade(s).
+    }
 
-				$this->prev_version = (string)$prev_version;
+    /**
+     * Runs upgrade handlers in the proper order.
+     *
+     * @since 141111 First documented version.
+     */
+    protected function runHandlers()
+    {
+        if (version_compare($this->prev_version, '141115', '<')) {
+            $this->fromLtV141115();
+        }
+    }
 
-				$this->run_handlers(); // Run upgrade(s).
-			}
+    /**
+     * Upgrading from a version prior to our rewrite.
+     *
+     * @since 141111 First documented version.
+     */
+    protected function fromLtV141115()
+    {
+        $sql1 = "ALTER TABLE `".esc_sql($this->plugin->utils_db->prefix().'subs')."`".
 
-			/**
-			 * Runs upgrade handlers in the proper order.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected function run_handlers()
-			{
-				if(version_compare($this->prev_version, '141115', '<'))
-					$this->from_lt_v141115();
-			}
+                " ADD `insertion_region` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic region code at time of insertion.',".
+                " ADD `insertion_country` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic country code at time of insertion.',".
 
-			/**
-			 * Upgrading from a version prior to our rewrite.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected function from_lt_v141115()
-			{
-				$sql1 = "ALTER TABLE `".esc_sql($this->plugin->utils_db->prefix().'subs')."`".
+                " ADD `last_region` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Last known geographic region code.',".
+                " ADD `last_country` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Last known geographic country code.'";
 
-				        " ADD `insertion_region` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic region code at time of insertion.',".
-				        " ADD `insertion_country` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic country code at time of insertion.',".
+        $sql2 = "ALTER TABLE `".esc_sql($this->plugin->utils_db->prefix().'sub_event_log')."`".
 
-				        " ADD `last_region` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Last known geographic region code.',".
-				        " ADD `last_country` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Last known geographic country code.'";
+                " ADD `region` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic region; at the time of the event.',".
+                " ADD `country` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic country; at the time of the event.',".
 
-				$sql2 = "ALTER TABLE `".esc_sql($this->plugin->utils_db->prefix().'sub_event_log')."`".
+                " ADD `region_before` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic region; before the event, if applicable.',".
+                " ADD `country_before` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic country; before the event, if applicable.'";
 
-				        " ADD `region` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic region; at the time of the event.',".
-				        " ADD `country` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic country; at the time of the event.',".
+        $sql3 = "ALTER TABLE `".esc_sql($this->plugin->utils_db->prefix().'queue_event_log')."`".
 
-				        " ADD `region_before` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic region; before the event, if applicable.',".
-				        " ADD `country_before` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic country; before the event, if applicable.'";
+                " ADD `region` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic region; at the time of the event.',".
+                " ADD `country` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic country; at the time of the event.'";
 
-				$sql3 = "ALTER TABLE `".esc_sql($this->plugin->utils_db->prefix().'queue_event_log')."`".
-
-				        " ADD `region` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic region; at the time of the event.',".
-				        " ADD `country` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic country; at the time of the event.'";
-
-				if($this->plugin->utils_db->wp->query($sql1) === FALSE
-				   || $this->plugin->utils_db->wp->query($sql2) === FALSE
-				   || $this->plugin->utils_db->wp->query($sql3) === FALSE
-				) throw new \exception(__('Query failure.', $this->plugin->text_domain));
-			}
-		}
+        if ($this->plugin->utils_db->wp->query($sql1) === false
+            || $this->plugin->utils_db->wp->query($sql2) === false
+            || $this->plugin->utils_db->wp->query($sql3) === false
+        ) {
+            throw new \exception(__('Query failure.', $this->plugin->text_domain));
+        }
+    }
+}
 	
