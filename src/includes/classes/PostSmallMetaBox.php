6efafd4373370e83e84b834512ac280cd6685d08
jaswsinc <jaswsinc@wsharks.com>
2016-02-13 23:29:01 -0900

    Refactor/format `PostSmallMetaBox`

--- src/includes/classes/PostSmallMetaBox.php
+++ src/includes/classes/PostSmallMetaBox.php
@@ -2,71 +2,72 @@
 /**
  * Post Small Meta Box
  *
- * @since 141111 First documented version.
+ * @since     141111 First documented version.
  * @copyright WebSharks, Inc. <http://www.websharks-inc.com>
- * @license GNU General Public License, version 3
+ * @license   GNU General Public License, version 3
  */
 namespace WebSharks\CommentMail\Pro;
 
+/**
+ * Post Small Meta Box
+ *
+ * @since 141111 First documented version.
+ */
+class PostSmallMetaBox extends AbsBase
+{
+    /**
+     * @var \WP_Post A WP post object.
+     *
+     * @since 141111 First documented version.
+     */
+    protected $post;
 
+    /**
+     * Class constructor.
+     *
+     * @since 141111 First documented version.
+     *
+     * @param \WP_Post $post A WP post object reference.
+     */
+    public function __construct(\WP_Post $post)
+    {
+        parent::__construct();
 
-		/**
-		 * Post Small Meta Box
-		 *
-		 * @since 141111 First documented version.
-		 */
-	class PostSmallMetaBox extends AbsBase
-		{
-			/**
-			 * @var \WP_Post A WP post object.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected $post;
+        $this->post = $post;
 
-			/**
-			 * Class constructor.
-			 *
-			 * @since 141111 First documented version.
-			 *
-			 * @param \WP_Post $post A WP post object reference.
-			 */
-			public function __construct(\WP_Post $post)
-			{
-				parent::__construct();
+        $this->display();
+    }
 
-				$this->post = $post;
+    /**
+     * Display meta box.
+     *
+     * @since 141111 First documented version.
+     */
+    protected function display()
+    {
+        $post_comment_status // Translate/standardize this.
+          = $this->plugin->utils_db->post_comment_status__($this->post->comment_status);
 
-				$this->display();
-			}
+        $total_subs        = $this->plugin->utils_sub->query_total($this->post->ID);
+        $total_subs_bubble = $this->plugin->utils_markup->subs_count(
+          $this->post->ID, $total_subs,
+          [
+            'subscriptions' => true,
+            'style'         => 'display:block; font-size:1.5em;',
+          ]
+        );
+        echo '<div class="'.esc_attr($this->plugin->slug.'-menu-page-area').'">'.
 
-			/**
-			 * Display meta box.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected function display()
-			{
-				$post_comment_status // Translate/standardize this.
-					= $this->plugin->utils_db->post_comment_status__($this->post->comment_status);
+             '  '.$total_subs_bubble. // In block format; i.e. 100% width.
 
-				$total_subs        = $this->plugin->utils_sub->query_total($this->post->ID);
-				$total_subs_bubble = $this->plugin->utils_markup->subs_count($this->post->ID, $total_subs,
-				                                                             array(
-					                                                             'subscriptions' => TRUE,
-					                                                             'style'         => 'display:block; font-size:1.5em;',
-				                                                             ));
-				echo '<div class="'.esc_attr($this->plugin->slug.'-menu-page-area').'">'.
+             '   <h4 style="margin:1em 0 .25em 0;">'.__('Most Recent Subscriptions', $this->plugin->text_domain).'</h4>'.
+             '   '.$this->plugin->utils_markup->last_x_subs(5, $this->post->ID, ['group_by_email' => true]).
 
-				     '  '.$total_subs_bubble. // In block format; i.e. 100% width.
+             '</div>';
 
-				     '   <h4 style="margin:1em 0 .25em 0;">'.__('Most Recent Subscriptions', $this->plugin->text_domain).'</h4>'.
-				     '   '.$this->plugin->utils_markup->last_x_subs(5, $this->post->ID, array('group_by_email' => TRUE)).
-
-				     '</div>';
-
-				if($post_comment_status !== 'open' && !$this->post->comment_count)
-					return; // For future implementation.
-			}
-		}
+        if ($post_comment_status !== 'open' && !$this->post->comment_count) {
+            return; // For future implementation.
+        }
+    }
+}
 	
