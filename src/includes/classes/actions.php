0f7d01b9f6889a6d3e6b0cce3fe70bde8f08befa
jaswsinc <jaswsinc@wsharks.com>
2016-02-14 13:42:48 -0900

    Removing PSR lint from AbsBase.

--- src/includes/classes/actions.php
+++ src/includes/classes/actions.php
@@ -1,15 +1,16 @@
 <?php
 /**
- * Actions
+ * Actions.
  *
  * @since     141111 First documented version.
+ *
  * @copyright WebSharks, Inc. <http://www.websharks-inc.com>
  * @license   GNU General Public License, version 3
  */
 namespace WebSharks\CommentMail\Pro;
 
 /**
- * Actions
+ * Actions.
  *
  * @since 141111 First documented version.
  *
