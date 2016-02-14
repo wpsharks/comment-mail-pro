9a728cbad3c3001971dabcc9213d7dc08d817d41
jaswsinc <jaswsinc@wsharks.com>
2016-02-14 01:11:36 -0900

    Refactor/format `SubManageSubNewForm`

--- src/includes/classes/SubManageSubNewForm.php
+++ src/includes/classes/SubManageSubNewForm.php
@@ -2,28 +2,26 @@
 /**
  * Sub. Management Sub. New Form
  *
- * @since 141111 First documented version.
+ * @since     141111 First documented version.
  * @copyright WebSharks, Inc. <http://www.websharks-inc.com>
- * @license GNU General Public License, version 3
+ * @license   GNU General Public License, version 3
  */
 namespace WebSharks\CommentMail\Pro;
 
-
-
-		/**
-		 * Sub. Management Sub. New Form
-		 *
-		 * @since 141111 First documented version.
-		 */
-	class SubManageSubNewForm extends SubManageSubFormBase
-		{
-			/**
-			 * Class constructor.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			public function __construct()
-			{
-				parent::__construct();
-			}
-		}
+/**
+ * Sub. Management Sub. New Form
+ *
+ * @since 141111 First documented version.
+ */
+class SubManageSubNewForm extends SubManageSubFormBase
+{
+    /**
+     * Class constructor.
+     *
+     * @since 141111 First documented version.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
