5ed12172fdc20b365de21905c57ccbbeebc10f5c
jaswsinc <jaswsinc@wsharks.com>
2016-02-14 00:46:45 -0900

    Refactor/format `SubInjector`

--- src/includes/classes/SubInjector.php
+++ src/includes/classes/SubInjector.php
@@ -2,198 +2,200 @@
 /**
  * Sub Injector
  *
- * @since 141111 First documented version.
+ * @since     141111 First documented version.
  * @copyright WebSharks, Inc. <http://www.websharks-inc.com>
- * @license GNU General Public License, version 3
+ * @license   GNU General Public License, version 3
  */
 namespace WebSharks\CommentMail\Pro;
 
+/**
+ * Sub Injector
+ *
+ * @since 141111 First documented version.
+ */
+class SubInjector extends AbsBase
+{
+    /**
+     * @var \WP_User|null Subscription.
+     *
+     * @since 141111 First documented version.
+     */
+    protected $user;
 
+    /**
+     * @var \stdClass|null Comment.
+     *
+     * @since 141111 First documented version.
+     */
+    protected $comment;
 
-		/**
-		 * Sub Injector
-		 *
-		 * @since 141111 First documented version.
-		 */
-	class SubInjector extends AbsBase
-		{
-			/**
-			 * @var \WP_User|null Subscription.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected $user;
+    /**
+     * @var string Subscription type.
+     *
+     * @since 141111 First documented version.
+     */
+    protected $type;
 
-			/**
-			 * @var \stdClass|null Comment.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected $comment;
+    /**
+     * @var string Subscription delivery option.
+     *
+     * @since 141111 First documented version.
+     */
+    protected $deliver;
 
-			/**
-			 * @var string Subscription type.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected $type;
+    /**
+     * @var null|boolean Auto-confirm?
+     *
+     * @since 141111 First documented version.
+     */
+    protected $auto_confirm;
 
-			/**
-			 * @var string Subscription delivery option.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected $deliver;
+    /**
+     * @var boolean Process events?
+     *
+     * @since 141111 First documented version.
+     */
+    protected $process_events;
 
-			/**
-			 * @var null|boolean Auto-confirm?
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected $auto_confirm;
+    /**
+     * @var boolean Process list server?
+     *
+     * @since 150922 Adding list server.
+     */
+    protected $process_list_server;
 
-			/**
-			 * @var boolean Process events?
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected $process_events;
+    /**
+     * @var boolean User initiated?
+     *
+     * @since 141111 First documented version.
+     */
+    protected $user_initiated;
 
-			/**
-			 * @var boolean Process list server?
-			 *
-			 * @since 150922 Adding list server.
-			 */
-			protected $process_list_server;
+    /**
+     * @var boolean Keep existing?
+     *
+     * @since 141111 First documented version.
+     */
+    protected $keep_existing;
 
-			/**
-			 * @var boolean User initiated?
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected $user_initiated;
+    /**
+     * @var sub_inserter|null Sub inserter.
+     *
+     * @since 141111 First documented version.
+     */
+    protected $sub_inserter;
 
-			/**
-			 * @var boolean Keep existing?
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected $keep_existing;
+    /**
+     * Class constructor.
+     *
+     * @since 141111 First documented version.
+     *
+     * @param \WP_User|null  $user       Subscribing user.
+     * @param integer|string $comment_id Comment ID.
+     * @param array          $args       Any additional behavioral args.
+     */
+    public function __construct(\WP_User $user = null, $comment_id = 0, array $args = [])
+    {
+        parent::__construct();
 
-			/**
-			 * @var sub_inserter|null Sub inserter.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected $sub_inserter;
+        $this->user = $user; // \WP_user|null.
 
-			/**
-			 * Class constructor.
-			 *
-			 * @since 141111 First documented version.
-			 *
-			 * @param \WP_User|null  $user Subscribing user.
-			 * @param integer|string $comment_id Comment ID.
-			 * @param array          $args Any additional behavioral args.
-			 */
-			public function __construct(\WP_User $user = NULL, $comment_id = 0, array $args = array())
-			{
-				parent::__construct();
+        $comment_id = (integer)$comment_id;
 
-				$this->user = $user; // \WP_user|null.
+        if ($comment_id) { // Need to have this.
+            $this->comment = get_comment($comment_id);
+        }
+        $defaults_args = [
+          'type'    => 'comment',
+          'deliver' => 'asap',
 
-				$comment_id = (integer)$comment_id;
+          'auto_confirm' => null,
 
-				if($comment_id) // Need to have this.
-					$this->comment = get_comment($comment_id);
+          'process_events'      => true,
+          'process_list_server' => false,
 
-				$defaults_args = array(
-					'type'                => 'comment',
-					'deliver'             => 'asap',
+          'user_initiated' => false,
 
-					'auto_confirm'        => NULL,
+          'keep_existing' => false,
+        ];
+        $args          = array_merge($defaults_args, $args);
+        $args          = array_intersect_key($args, $defaults_args);
 
-					'process_events'      => TRUE,
-					'process_list_server' => FALSE,
+        $this->type    = trim(strtolower((string)$args['type']));
+        $this->deliver = trim(strtolower((string)$args['deliver']));
+        $this->deliver = !$this->deliver ? 'asap' : $this->deliver;
 
-					'user_initiated'      => FALSE,
+        if (isset($args['auto_confirm'])) {
+            $this->auto_confirm = (boolean)$args['auto_confirm'];
+        }
+        $this->process_events      = (boolean)$args['process_events'];
+        $this->process_list_server = (boolean)$args['process_list_server'];
 
-					'keep_existing'       => FALSE,
-				);
-				$args          = array_merge($defaults_args, $args);
-				$args          = array_intersect_key($args, $defaults_args);
+        $this->user_initiated = (boolean)$args['user_initiated'];
+        $this->user_initiated = $this->plugin->utils_sub->check_user_initiated_by_admin(
+          $this->comment ? $this->comment->comment_author_email : '', $this->user_initiated
+        );
+        $this->keep_existing  = (boolean)$args['keep_existing'];
 
-				$this->type    = trim(strtolower((string)$args['type']));
-				$this->deliver = trim(strtolower((string)$args['deliver']));
-				$this->deliver = !$this->deliver ? 'asap' : $this->deliver;
+        $this->maybeInject();
+    }
 
-				if(isset($args['auto_confirm']))
-					$this->auto_confirm    = (boolean)$args['auto_confirm'];
+    /**
+     * Sub inserter.
+     *
+     * @since 141111 First documented version.
+     *
+     * @return sub_inserter|null Sub inserter.
+     */
+    public function subInserter()
+    {
+        return $this->sub_inserter;
+    }
 
-				$this->process_events      = (boolean)$args['process_events'];
-				$this->process_list_server = (boolean)$args['process_list_server'];
+    /**
+     * Injects a new subscription.
+     *
+     * @since 141111 First documented version.
+     */
+    protected function maybeInject()
+    {
+        if (!$this->comment) {
+            return; // Not possible.
+        }
+        if (!$this->comment->comment_post_ID) {
+            return; // Not possible.
+        }
+        if (!$this->comment->comment_ID) {
+            return; // Not possible.
+        }
+        if (!$this->comment->comment_author_email) {
+            return; // Not possible.
+        }
+        if ($this->comment->comment_type
+            && $this->comment->comment_type !== 'comment'
+        ) {
+            return; // Not applicable.
+        }
+        $data               = [
+          'post_id'    => $this->comment->comment_post_ID,
+          'user_id'    => $this->user ? $this->user->ID : null,
+          'comment_id' => $this->type === 'comments' ? 0 : $this->comment->comment_ID,
+          'deliver'    => $this->deliver, // Delivery option.
 
-				$this->user_initiated      = (boolean)$args['user_initiated'];
-				$this->user_initiated      = $this->plugin->utils_sub->check_user_initiated_by_admin(
-					$this->comment ? $this->comment->comment_author_email : '', $this->user_initiated
-				);
-				$this->keep_existing       = (boolean)$args['keep_existing'];
-
-				$this->maybe_inject();
-			}
-
-			/**
-			 * Sub inserter.
-			 *
-			 * @since 141111 First documented version.
-			 *
-			 * @return sub_inserter|null Sub inserter.
-			 */
-			public function sub_inserter()
-			{
-				return $this->sub_inserter;
-			}
-
-			/**
-			 * Injects a new subscription.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected function maybe_inject()
-			{
-				if(!$this->comment)
-					return; // Not possible.
-
-				if(!$this->comment->comment_post_ID)
-					return; // Not possible.
-
-				if(!$this->comment->comment_ID)
-					return; // Not possible.
-
-				if(!$this->comment->comment_author_email)
-					return; // Not possible.
-
-				if($this->comment->comment_type
-				   && $this->comment->comment_type !== 'comment'
-				) return; // Not applicable.
-
-				$data               = array(
-					'post_id'    => $this->comment->comment_post_ID,
-					'user_id'    => $this->user ? $this->user->ID : NULL,
-					'comment_id' => $this->type === 'comments' ? 0 : $this->comment->comment_ID,
-					'deliver'    => $this->deliver, // Delivery option.
-
-					'fname'      => $this->plugin->utils_string->first_name($this->comment->comment_author, $this->comment->comment_author_email),
-					'lname'      => $this->plugin->utils_string->last_name($this->comment->comment_author),
-					'email'      => $this->comment->comment_author_email,
-				);
-				$this->sub_inserter = new SubInserter($data, array(
-					'process_confirmation' => TRUE, // Always.
-					'auto_confirm'         => $this->auto_confirm,
-					'process_events'       => $this->process_events,
-					'process_list_server'  => $this->process_list_server,
-					'user_initiated'       => $this->user_initiated,
-					'keep_existing'        => $this->keep_existing,
-				));
-			}
-		}
+          'fname' => $this->plugin->utils_string->first_name($this->comment->comment_author, $this->comment->comment_author_email),
+          'lname' => $this->plugin->utils_string->last_name($this->comment->comment_author),
+          'email' => $this->comment->comment_author_email,
+        ];
+        $this->sub_inserter = new SubInserter(
+          $data,
+          [
+            'process_confirmation' => true, // Always.
+            'auto_confirm'         => $this->auto_confirm,
+            'process_events'       => $this->process_events,
+            'process_list_server'  => $this->process_list_server,
+            'user_initiated'       => $this->user_initiated,
+            'keep_existing'        => $this->keep_existing,
+          ]
+        );
+    }
+}
