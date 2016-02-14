aa2bbbcaceba0dc97604efd3c17130af999740cd
jaswsinc <jaswsinc@wsharks.com>
2016-02-13 23:58:55 -0900

    Refactor/format `SsoActions`

--- src/includes/classes/SsoActions.php
+++ src/includes/classes/SsoActions.php
@@ -2,103 +2,106 @@
 /**
  * SSO Actions
  *
- * @since 141111 First documented version.
+ * @since     141111 First documented version.
  * @copyright WebSharks, Inc. <http://www.websharks-inc.com>
- * @license GNU General Public License, version 3
+ * @license   GNU General Public License, version 3
  */
 namespace WebSharks\CommentMail\Pro;
 
+/**
+ * SSO Actions
+ *
+ * @since 141111 First documented version.
+ */
+class SsoActions extends AbsBase
+{
+    /**
+     * @var array Valid actions.
+     *
+     * @since 141111 First documented version.
+     */
+    protected $valid_actions;
 
+    /**
+     * @var array Valid services.
+     *
+     * @since 141111 First documented version.
+     */
+    public static $valid_services = [
+      'twitter',
+      'facebook',
+      'google',
+      'linkedin',
+    ];
 
-		/**
-		 * SSO Actions
-		 *
-		 * @since 141111 First documented version.
-		 */
-	class SsoActions extends AbsBase
-		{
-			/**
-			 * @var array Valid actions.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected $valid_actions;
+    /**
+     * Class constructor.
+     *
+     * @since 141111 First documented version.
+     */
+    public function __construct()
+    {
+        parent::__construct();
 
-			/**
-			 * @var array Valid services.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			public static $valid_services = array(
-				'twitter',
-				'facebook',
-				'google',
-				'linkedin',
-			);
+        $this->valid_actions = [
+          'sso',
+        ];
+        $this->maybeHandle();
+    }
 
-			/**
-			 * Class constructor.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			public function __construct()
-			{
-				parent::__construct();
+    /**
+     * Action handler.
+     *
+     * @since 141111 First documented version.
+     */
+    protected function maybeHandle()
+    {
+        if (is_admin()) {
+            return; // Not applicable.
+        }
+        if (empty($_REQUEST[GLOBAL_NS])) {
+            return; // Not applicable.
+        }
+        $cb_r_args = []; // Initialize callback request args.
+        $_r        = $this->plugin->utils_string->trim_strip_deep($_REQUEST);
 
-				$this->valid_actions
-					= array(
-					'sso',
-				);
-				$this->maybe_handle();
-			}
+        foreach (['oauth_token', 'oauth_verifier', 'oauth_problem', 'code', 'state'] as $_cb_r_arg_key) {
+            if (isset($_r[$_cb_r_arg_key])) {
+                $cb_r_args[$_cb_r_arg_key] = $_r[$_cb_r_arg_key];
+            }
+        }
+        unset($_cb_r_arg_key); // Housekeeping.
 
-			/**
-			 * Action handler.
-			 *
-			 * @since 141111 First documented version.
-			 */
-			protected function maybe_handle()
-			{
-				if(is_admin())
-					return; // Not applicable.
+        foreach ((array)$_REQUEST[GLOBAL_NS] as $_action => $_request_args) {
+            if ($_action && in_array($_action, $this->valid_actions, true) && is_array($_request_args)) {
+                $_method = preg_replace_callback('/_(.)/', function ($m) { return strtoupper($m[1]); }, strtolower($_action));
+                $this->{$_method}(array_merge($cb_r_args, $this->plugin->utils_string->trim_strip_deep($_request_args)));
+            }
+        }
+        unset($_action, $_method, $_request_args); // Housekeeping.
+    }
 
-				if(empty($_REQUEST[GLOBAL_NS]))
-					return; // Not applicable.
+    /**
+     * SSO actions for various services.
+     *
+     * @since 141111 First documented version.
+     *
+     * @param mixed $request_args Input argument(s).
+     */
+    protected function sso(array $request_args)
+    {
+        if (empty($request_args['service'])) {
+            return; // Empty service identifier.
+        }
+        if (!in_array($request_args['service'], static::$valid_services, true)) {
+            return; // Invalid import type.
+        }
+        if (!class_exists($class = '\\'.__NAMESPACE__.'\\Sso'.ucfirst($request_args['service']))) {
+            return; // Invalid service identifier.
+        }
+        new $class($request_args);
 
-				$cb_r_args = array(); // Initialize callback request args.
-				$_r        = $this->plugin->utils_string->trim_strip_deep($_REQUEST);
-
-				foreach(array('oauth_token', 'oauth_verifier', 'oauth_problem', 'code', 'state') as $_cb_r_arg_key)
-					if(isset($_r[$_cb_r_arg_key])) $cb_r_args[$_cb_r_arg_key] = $_r[$_cb_r_arg_key];
-				unset($_cb_r_arg_key); // Housekeeping.
-
-				foreach((array)$_REQUEST[GLOBAL_NS] as $_action => $_request_args)
-					if($_action && in_array($_action, $this->valid_actions, TRUE) && is_array($_request_args))
-						$this->{$_action}(array_merge($cb_r_args, $this->plugin->utils_string->trim_strip_deep($_request_args)));
-				unset($_action, $_request_args); // Housekeeping.
-			}
-
-			/**
-			 * SSO actions for various services.
-			 *
-			 * @since 141111 First documented version.
-			 *
-			 * @param mixed $request_args Input argument(s).
-			 */
-			protected function sso(array $request_args)
-			{
-				if(empty($request_args['service']))
-					return; // Empty service identifier.
-
-				if(!in_array($request_args['service'], static::$valid_services, TRUE))
-					return; // Invalid import type.
-
-				if(!class_exists($class = '\\'.__NAMESPACE__.'\\sso_'.$request_args['service']))
-					return; // Invalid service identifier.
-
-				new $class($request_args);
-
-				exit(); // Stop; always.
-			}
-		}
+        exit(); // Stop; always.
+    }
+}
 	
