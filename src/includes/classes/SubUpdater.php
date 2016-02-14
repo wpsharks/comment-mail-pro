31ebb389ca9dc56f0a289bd1505e882685b7755d
jaswsinc <jaswsinc@wsharks.com>
2016-02-14 01:15:02 -0900

    Refactor/format `SubUpdater`

--- src/includes/classes/SubUpdater.php
+++ src/includes/classes/SubUpdater.php
@@ -2,36 +2,34 @@
 /**
  * Sub Updater
  *
- * @since 141111 First documented version.
+ * @since     141111 First documented version.
  * @copyright WebSharks, Inc. <http://www.websharks-inc.com>
- * @license GNU General Public License, version 3
+ * @license   GNU General Public License, version 3
  */
 namespace WebSharks\CommentMail\Pro;
 
-
-
-		/**
-		 * Sub Updater
-		 *
-		 * @since 141111 First documented version.
-		 */
-	class SubUpdater extends SubInserter
-		{
-			/**
-			 * Class constructor.
-			 *
-			 * @since 141111 First documented version.
-			 *
-			 * @param array $request_args Arguments to the constructor.
-			 *    These should NOT be trusted; they come from a `$_REQUEST` action.
-			 *
-			 * @param array $args Any additional behavioral args.
-			 */
-			public function __construct(array $request_args, array $args = array())
-			{
-				if(!isset($request_args['ID']))
-					$request_args['ID'] = -1;
-
-				parent::__construct($request_args, $args);
-			}
-		}
+/**
+ * Sub Updater
+ *
+ * @since 141111 First documented version.
+ */
+class SubUpdater extends SubInserter
+{
+    /**
+     * Class constructor.
+     *
+     * @since 141111 First documented version.
+     *
+     * @param array $request_args Arguments to the constructor.
+     *                            These should NOT be trusted; they come from a `$_REQUEST` action.
+     *
+     * @param array $args         Any additional behavioral args.
+     */
+    public function __construct(array $request_args, array $args = [])
+    {
+        if (!isset($request_args['ID'])) {
+            $request_args['ID'] = -1;
+        }
+        parent::__construct($request_args, $args);
+    }
+}
