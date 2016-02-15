98ed8dff9598208abccca3560fd2675f0e329e35
jaswsinc <jaswsinc@wsharks.com>
2016-02-14 15:17:16 -0900

    Removing PSR lint from `Upgrader`

--- src/includes/classes/upgrader.php
+++ src/includes/classes/upgrader.php
@@ -1,22 +1,23 @@
 <?php
 /**
- * Upgrade Routines
+ * Upgrade Routines.
  *
  * @since     141111 First documented version.
+ *
  * @copyright WebSharks, Inc. <http://www.websharks-inc.com>
  * @license   GNU General Public License, version 3
  */
 namespace WebSharks\CommentMail\Pro;
 
 /**
- * Upgrade Routines
+ * Upgrade Routines.
  *
  * @since 141111 First documented version.
  */
 class Upgrader extends AbsBase
 {
     /**
-     * @var string Previous version.
+     * @type string Previous version.
      *
      * @since 141111 First documented version.
      */
@@ -51,7 +52,7 @@ class Upgrader extends AbsBase
 
         new UpgraderVs($this->prev_version); // Run version-specific upgrader(s).
 
-        $this->plugin->enqueueNotice( // Notify site owner about this upgrade process.
+        $this->plugin->enqueueNotice(// Notify site owner about this upgrade process.
             sprintf(__('<strong>%1$s&trade;</strong> was automatically recompiled upon detecting an upgrade to v%2$s. Your existing configuration remains :-)', $this->plugin->text_domain), esc_html($this->plugin->name), esc_html($this->plugin->version)),
             ['requires_cap' => $this->plugin->auto_recompile_cap, 'push_to_top' => true]
         );
