0ff610e8458ded445f2105cd055830038e6c54d2
jaswsinc <jaswsinc@wsharks.com>
2016-02-14 15:12:34 -0900

    Removing PSR lint from `SubInjector`

--- src/includes/classes/SubInjector.php
+++ src/includes/classes/SubInjector.php
@@ -1,85 +1,86 @@
 <?php
 /**
- * Sub Injector
+ * Sub Injector.
  *
  * @since     141111 First documented version.
+ *
  * @copyright WebSharks, Inc. <http://www.websharks-inc.com>
  * @license   GNU General Public License, version 3
  */
 namespace WebSharks\CommentMail\Pro;
 
 /**
- * Sub Injector
+ * Sub Injector.
  *
  * @since 141111 First documented version.
  */
 class SubInjector extends AbsBase
 {
     /**
-     * @var \WP_User|null Subscription.
+     * @type \WP_User|null Subscription.
      *
      * @since 141111 First documented version.
      */
     protected $user;
 
     /**
-     * @var \stdClass|null Comment.
+     * @type \stdClass|null Comment.
      *
      * @since 141111 First documented version.
      */
     protected $comment;
 
     /**
-     * @var string Subscription type.
+     * @type string Subscription type.
      *
      * @since 141111 First documented version.
      */
     protected $type;
 
     /**
-     * @var string Subscription delivery option.
+     * @type string Subscription delivery option.
      *
      * @since 141111 First documented version.
      */
     protected $deliver;
 
     /**
-     * @var null|boolean Auto-confirm?
+     * @type null|bool Auto-confirm?
      *
      * @since 141111 First documented version.
      */
     protected $auto_confirm;
 
     /**
-     * @var boolean Process events?
+     * @type bool Process events?
      *
      * @since 141111 First documented version.
      */
     protected $process_events;
 
     /**
-     * @var boolean Process list server?
+     * @type bool Process list server?
      *
      * @since 150922 Adding list server.
      */
     protected $process_list_server;
 
     /**
-     * @var boolean User initiated?
+     * @type bool User initiated?
      *
      * @since 141111 First documented version.
      */
     protected $user_initiated;
 
     /**
-     * @var boolean Keep existing?
+     * @type bool Keep existing?
      *
      * @since 141111 First documented version.
      */
     protected $keep_existing;
 
     /**
-     * @var sub_inserter|null Sub inserter.
+     * @type sub_inserter|null Sub inserter.
      *
      * @since 141111 First documented version.
      */
@@ -90,9 +91,9 @@ class SubInjector extends AbsBase
      *
      * @since 141111 First documented version.
      *
-     * @param \WP_User|null  $user       Subscribing user.
-     * @param integer|string $comment_id Comment ID.
-     * @param array          $args       Any additional behavioral args.
+     * @param \WP_User|null $user       Subscribing user.
+     * @param int|string    $comment_id Comment ID.
+     * @param array         $args       Any additional behavioral args.
      */
     public function __construct(\WP_User $user = null, $comment_id = 0, array $args = [])
     {
@@ -100,7 +101,7 @@ class SubInjector extends AbsBase
 
         $this->user = $user; // \WP_user|null.
 
-        $comment_id = (integer)$comment_id;
+        $comment_id = (integer) $comment_id;
 
         if ($comment_id) { // Need to have this.
             $this->comment = get_comment($comment_id);
@@ -118,24 +119,25 @@ class SubInjector extends AbsBase
 
             'keep_existing' => false,
         ];
-        $args          = array_merge($defaults_args, $args);
-        $args          = array_intersect_key($args, $defaults_args);
+        $args = array_merge($defaults_args, $args);
+        $args = array_intersect_key($args, $defaults_args);
 
-        $this->type    = trim(strtolower((string)$args['type']));
-        $this->deliver = trim(strtolower((string)$args['deliver']));
+        $this->type    = trim(strtolower((string) $args['type']));
+        $this->deliver = trim(strtolower((string) $args['deliver']));
         $this->deliver = !$this->deliver ? 'asap' : $this->deliver;
 
         if (isset($args['auto_confirm'])) {
-            $this->auto_confirm = (boolean)$args['auto_confirm'];
+            $this->auto_confirm = (boolean) $args['auto_confirm'];
         }
-        $this->process_events      = (boolean)$args['process_events'];
-        $this->process_list_server = (boolean)$args['process_list_server'];
+        $this->process_events      = (boolean) $args['process_events'];
+        $this->process_list_server = (boolean) $args['process_list_server'];
 
-        $this->user_initiated = (boolean)$args['user_initiated'];
+        $this->user_initiated = (boolean) $args['user_initiated'];
         $this->user_initiated = $this->plugin->utils_sub->checkUserInitiatedByAdmin(
-            $this->comment ? $this->comment->comment_author_email : '', $this->user_initiated
+            $this->comment ? $this->comment->comment_author_email : '',
+            $this->user_initiated
         );
-        $this->keep_existing  = (boolean)$args['keep_existing'];
+        $this->keep_existing = (boolean) $args['keep_existing'];
 
         $this->maybeInject();
     }
@@ -176,7 +178,7 @@ class SubInjector extends AbsBase
         ) {
             return; // Not applicable.
         }
-        $data               = [
+        $data = [
             'post_id'    => $this->comment->comment_post_ID,
             'user_id'    => $this->user ? $this->user->ID : null,
             'comment_id' => $this->type === 'comments' ? 0 : $this->comment->comment_ID,
