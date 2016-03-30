be5b9b2657619da156c08a52bebcf1373011b52c
kristineds <kristine@websharks-inc.com>
2016-03-30 21:09:16 +0800

    Fix conditional statement for No subscriptions and add View link; See: websharks/comment-mail#231

--- src/includes/classes/UtilsI18n.php
+++ src/includes/classes/UtilsI18n.php
@@ -353,10 +353,11 @@ class UtilsI18n extends AbsBase
         $counter = (integer) $counter; // Force integer.
 
         if (empty($counter)) { // If no results, add a no subscriptions message.
-            $counter = ' '.__('No Subscriptions (View)', $counter, SLUG_TD);
-
+            return sprintf(_n('No Subscriptions (View)','No Subscriptions (View)', $counter, SLUG_TD), $counter);
         }
-        return sprintf(_n('%1$s Subscriptions Total (View All)','%1$s Subscriptions Total (View All)', $counter, SLUG_TD), $counter);
+        else {
+            return sprintf(_n('%1$s Subscriptions Total (View All)','%1$s Subscriptions Total (View All)', $counter, SLUG_TD), $counter);
+        }
     }
 
     /**
