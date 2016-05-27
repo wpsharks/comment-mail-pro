cd1fe50b4e52c5011686358cda8602f0cd4983a5
Raam Dev <raam@websharks-inc.com>
2016-05-27 15:25:24 -0400

    Backup customized Advanced Templates and then reset them to defaults
    See websharks/comment-mail#238

--- src/includes/classes/Template.php
+++ src/includes/classes/Template.php
@@ -334,6 +334,9 @@ class Template extends AbsBase
         $option_key = static::dataOptionKey(['type' => $this->type, 'file' => $this->file]);
 
         if (!empty($this->plugin->options[$option_key])) {
+            // Strip legacy template backup, if applicable
+            $this->plugin->options[$option_key] = preg_replace('/\<\?php\s+\/\* --------------------------- Legacy Template Backup ---------------------------.*/su', '', $this->plugin->options[$option_key]);
+
             return $this->plugin->options[$option_key];
         }
         default_template: // Target point; default template.
--- src/includes/classes/UpgraderVs.php
+++ src/includes/classes/UpgraderVs.php
@@ -46,9 +46,8 @@ class UpgraderVs extends AbsBase
      */
     protected function runHandlers()
     {
-        if (version_compare($this->prev_version, '141115', '<')) {
-            $this->fromLtV141115();
-        }
+        $this->fromLtV141115();
+        $this->fromLteV160213();
     }
 
     /**
@@ -58,32 +57,76 @@ class UpgraderVs extends AbsBase
      */
     protected function fromLtV141115()
     {
-        $sql1 = 'ALTER TABLE `'.esc_sql($this->plugin->utils_db->prefix().'subs').'`'.
+        if (version_compare($this->prev_version, '141115', '<')) {
+            $sql1 = 'ALTER TABLE `'.esc_sql($this->plugin->utils_db->prefix().'subs').'`'.
 
-                " ADD `insertion_region` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic region code at time of insertion.',".
-                " ADD `insertion_country` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic country code at time of insertion.',".
+                    " ADD `insertion_region` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic region code at time of insertion.',".
+                    " ADD `insertion_country` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic country code at time of insertion.',".
 
-                " ADD `last_region` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Last known geographic region code.',".
-                " ADD `last_country` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Last known geographic country code.'";
+                    " ADD `last_region` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Last known geographic region code.',".
+                    " ADD `last_country` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Last known geographic country code.'";
 
-        $sql2 = 'ALTER TABLE `'.esc_sql($this->plugin->utils_db->prefix().'sub_event_log').'`'.
+            $sql2 = 'ALTER TABLE `'.esc_sql($this->plugin->utils_db->prefix().'sub_event_log').'`'.
 
-                " ADD `region` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic region; at the time of the event.',".
-                " ADD `country` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic country; at the time of the event.',".
+                    " ADD `region` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic region; at the time of the event.',".
+                    " ADD `country` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic country; at the time of the event.',".
 
-                " ADD `region_before` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic region; before the event, if applicable.',".
-                " ADD `country_before` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic country; before the event, if applicable.'";
+                    " ADD `region_before` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic region; before the event, if applicable.',".
+                    " ADD `country_before` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic country; before the event, if applicable.'";
 
-        $sql3 = 'ALTER TABLE `'.esc_sql($this->plugin->utils_db->prefix().'queue_event_log').'`'.
+            $sql3 = 'ALTER TABLE `'.esc_sql($this->plugin->utils_db->prefix().'queue_event_log').'`'.
 
-                " ADD `region` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic region; at the time of the event.',".
-                " ADD `country` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic country; at the time of the event.'";
+                    " ADD `region` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic region; at the time of the event.',".
+                    " ADD `country` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Geographic country; at the time of the event.'";
 
-        if ($this->plugin->utils_db->wp->query($sql1) === false
-            || $this->plugin->utils_db->wp->query($sql2) === false
-            || $this->plugin->utils_db->wp->query($sql3) === false
-        ) {
-            throw new \exception(__('Query failure.', SLUG_TD));
+            if ($this->plugin->utils_db->wp->query($sql1) === false
+                || $this->plugin->utils_db->wp->query($sql2) === false
+                || $this->plugin->utils_db->wp->query($sql3) === false
+            ) {
+                throw new \exception(__('Query failure.', SLUG_TD));
+            }
+        }
+    }
+
+    /**
+     * Upgrading from a version prior to changes that broke back. compat. w/ Advanced Templates
+     *
+     * @since 16xxxx First documented version.
+     */
+    protected function fromLteV160213()
+    {
+        if (version_compare($this->prev_version, '160213', '<=')) {
+
+            $_marker = '<?php /* --------------------------- Legacy Template Backup ---------------------------
+ * Comment Mail v16xxxx included changes that were not backwards compatible with your 
+ * customized Advanced Template. To prevent problems with the upgrade to v16xxxx, we reset
+ * the Advanced Templates to their (new) default and backed up your customized template, a
+ * copy of which is included below. You can reference your original template below to reapply
+ * those changes to the new default template above. When you are ready to discard this backup,
+ * simply delete this comment, and everything below it, and save the template. If you leave 
+ * this comment here and save your the options, your backup below will be also saved.
+ * 
+ * Note: Everything below this comment is not parsed by Comment Mail; it is only here for
+ * your reference so that you can re-apply your modifications to the new template above.
+ */ ?>';
+
+            foreach ($this->plugin->options as $_key => &$_value) {
+                if (strpos($_key, 'template__') === 0) {
+                    $_key_data             = Template::optionKeyData($_key);
+                    $_default_template     = new Template($_key_data->file, $_key_data->type, true);
+                    $_default_template_nws = $_default_template->fileContents();
+                    $_new_and_old_template = $_default_template_nws ."\n\n". $_marker ."\n\n". $_value;
+
+                    $this->plugin->options[$_key] = $_new_and_old_template;
+
+                }
+            }
+            unset($_marker, $_key, $_key_data, $_value); // Housekeeping.
+            unset($_default_template, $_default_template_nws, $_new_and_old_template);
+
+
+            $this->plugin->optionsSave($this->plugin->options); // Update with backed up Advanced Templates
+
         }
     }
 }
