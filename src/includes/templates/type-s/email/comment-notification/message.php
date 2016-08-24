2fada97af328ea50f4b7baa5b47aaf98d9b70fa1
Raam Dev <raam@websharks-inc.com>
2016-08-24 15:22:13 -0400

    Fix fatal error in Lite version from calling Pro-only RVE utility
    See websharks/comment-mail#297

--- src/includes/templates/type-s/email/comment-notification/message.php
+++ src/includes/templates/type-s/email/comment-notification/message.php
@@ -58,9 +58,10 @@ $sub_last_update_time_ago = $plugin->utils_date->i18nUtc('M jS, Y @ g:i a T', $s
 
 // A notification may contain one (or more) comments. Is this a digest?
 $is_digest = count($comments) > 1; // `TRUE`, if more than one comment in the notification.
-
+/*[pro strip-from="lite"]*/
 // Plugin is configured to allow replies via email? If so, this will be `TRUE`.
 $replies_via_email_enable = $sub_post_comments_open && $plugin->options['replies_via_email_enable'];
+/*[/pro]*/
 ?>
 <?php echo $template->snippet(
     'message-heading.php',
@@ -103,10 +104,11 @@ $replies_via_email_enable = $sub_post_comments_open && $plugin->options['replies
 
         // A shorter clip of the full comment message body; in plain text.
         $_comment_clip = $plugin->utils_markup->commentContentClip($_comment, 'notification', false);
-
+        /*[pro strip-from="lite"]*/
         // Reply via email marker; if applicable. Only needed for digests, and only if replies via email are enabled currently.
         // ~ Note: This marker is not necessary for single comment notifications. A `Reply-To:` header already handles single-comment notifications.
         $_comment_rve_irt_marker = $plugin->utils_rve->irtMarker($_comment->comment_post_ID, $_comment->comment_ID); // e.g. `~rve#779-84`.
+        /*[/pro]*/
         ?>
         <?php if ($_comment_parent) : // This is a reply to someone? ?>
 
@@ -143,6 +145,7 @@ $replies_via_email_enable = $sub_post_comments_open && $plugin->options['replies
                             <?php echo __('Reply', SLUG_TD); ?>
                         <?php endif; ?>
                     </a>
+                    <?php /*[pro strip-from="lite"]*/ ?>
                     <?php if ($replies_via_email_enable) : ?>
                         <?php if ($is_digest) : // Marker only needed in digests. ?>
                             <small><em><?php echo sprintf(__('— or reply to this email &amp; start your message with: <code>%1$s</code>', SLUG_TD), esc_html($_comment_rve_irt_marker)); ?></em></small>
@@ -150,6 +153,7 @@ $replies_via_email_enable = $sub_post_comments_open && $plugin->options['replies
                             <small><em><?php echo __('— or simply reply to this email', SLUG_TD); ?></em></small>
                         <?php endif; ?>
                     <?php endif; ?>
+                    <?php /*[/pro]*/ ?>
                 <?php endif; ?>
             </p>
 
@@ -177,6 +181,7 @@ $replies_via_email_enable = $sub_post_comments_open && $plugin->options['replies
                             <?php echo __('Reply', SLUG_TD); ?>
                         <?php endif; ?>
                     </a>
+                    <?php /*[pro strip-from="lite"]*/ ?>
                     <?php if ($replies_via_email_enable) : ?>
                         <?php if ($is_digest) : // Marker only needed in digests. ?>
                             <small><em><?php echo sprintf(__('— or reply to this email &amp; start your message with: <code>%1$s</code>', SLUG_TD), esc_html($_comment_rve_irt_marker)); ?></em></small>
@@ -185,6 +190,7 @@ $replies_via_email_enable = $sub_post_comments_open && $plugin->options['replies
                             <small><strong><?php echo __('Please Note:', SLUG_TD); ?></strong> <em><?php echo __('Your reply will be posted publicly and immediately.', SLUG_TD); ?></em></small>
                         <?php endif; ?>
                     <?php endif; ?>
+                    <?php /*[/pro]*/ ?>
                 <?php endif; ?>
             </p>
 
