f42a52f6f9db6e1b638005ade354726e81af4718
jaswsinc <me@jaswsinc.com>
2016-11-04 08:36:35 -0800

    SparkPost completion. See: websharks/comment-mail#265

--- src/includes/classes/RveSparkPost.php
+++ src/includes/classes/RveSparkPost.php
@@ -98,8 +98,7 @@ class RveSparkPost extends AbsBase
      */
     protected function collectMessage()
     {
-        $response = file_get_contents('php://input');
-        $response = json_decode((string) $response);
+        $response = json_decode((string) file_get_contents('php://input'));
 
         if (empty($response[0]->msys->relay_message)) {
             return; // Nothing to do.
@@ -116,70 +115,55 @@ class RveSparkPost extends AbsBase
      */
     protected function processMessage()
     {
-        foreach ($this->events as $_event) {
-            // Iterate all events.
-
-            if (empty($_event->ts) || $_event->ts < strtotime('-7 days')) {
-                continue; // Missing timestamp; or it's very old.
-            }
-            if (empty($_event->event) || $_event->event !== 'inbound') {
-                continue; // Expecting an inbound event.
-            }
-            if (empty($_event->msg) || !($_event->msg instanceof \stdClass)) {
-                continue; // Expecting a msg object w/ properties.
-            }
-            $_reply_to_email = $this->issetOr($_event->msg->email, '', 'string');
-
-            $_from_name  = $this->issetOr($_event->msg->from_name, '', 'string');
-            $_from_email = $this->issetOr($_event->msg->from_email, '', 'string');
-
-            $_subject = $this->issetOr($_event->msg->subject, '', 'string');
-
-            $_text_body = $this->issetOr($_event->msg->text, '', 'string');
-            $_html_body = $this->issetOr($_event->msg->html, '', 'string');
-
-            if (isset($_event->msg->spam_report->score)) {
-                $_spam_score = (float) $_event->msg->spam_report->score;
-            } else {
-                $_spam_score = 0.0; // Default value.
-            }
-            if (isset($_event->msg->spf->result)) {
-                $_spf_result = strtolower((string) $_event->msg->spf->result);
-            } else {
-                $_spf_result = 'none'; // Default value.
-            }
-            if (isset($_event->msg->dkim->signed)) {
-                $_dkim_signed = (bool) $_event->msg->dkim->signed;
-            } else {
-                $_dkim_signed = false; // Default value.
-            }
-            if (isset($_event->msg->dkim->valid)) {
-                $_dkim_valid = (bool) $_event->msg->dkim->valid;
-            } else {
-                $_dkim_valid = false; // Default value.
-            }
-            $this->maybeProcessCommentReply(
-                [
-                    'reply_to_email' => $_reply_to_email,
-
-                    'from_name'  => $_from_name,
-                    'from_email' => $_from_email,
-
-                    'subject' => $_subject,
-
-                    'text_body' => $_text_body,
-                    'html_body' => $_html_body,
-
-                    'spam_score' => $_spam_score,
-
-                    'spf_result' => $_spf_result,
-
-                    'dkim_signed' => $_dkim_signed,
-                    'dkim_valid'  => $_dkim_valid,
-                ]
-            );
+        if (!$this->message) {
+            return; // Not possible.
+        } elseif (empty($this->message->protocol)) {
+            return; // Not possible.
+        } elseif (empty($this->message->webhook_id)) {
+            return; // Not possible.
+        } elseif ($this->message->protocol !== 'smtp') {
+            return; // Not possible.
+        } elseif ($this->message->webhook_id !== $this->plugin->options['rve_sparkpost_webhook_id']) {
+            return; // Webhook ID does not match.
+        } elseif (empty($this->message->content->headers)) {
+            return; // Not possible.
         }
-        unset($_event); // Housekeeping.
+        $headers = []; // Initialize array of all headers.
+
+        foreach ($this->message->content->headers as $_header) {
+            $headers = array_merge($headers, (array) $_header);
+        } // unset($_header); // Housekeeping.
+
+        if (empty($headers['To']) || empty($headers['From'])) {
+            return; // Not possible; missing headers.
+        } elseif (!($to_addresses = array_values($this->plugin->utils_mail->parseAddressesDeep($headers['To'])))) {
+            return; // Not possible; no recipient addresses.
+        } elseif (!($from_addresses = array_values($this->plugin->utils_mail->parseAddressesDeep($headers['From'])))) {
+            return; // Not possible; no from addresses.
+        }
+        $reply_to_email = $to_addresses[0]->email;
+
+        $from_name  = trim($from_addresses[0]->fname.' '.$from_addresses[0]->lname);
+        $from_email = $from_addresses[0]->email;
+
+        $subject = $this->message->content->subject;
+
+        $text_body = $this->message->content->text;
+        $html_body = $this->message->content->html;
+
+        $this->maybeProcessCommentReply(
+            [
+                'reply_to_email' => $reply_to_email,
+
+                'from_name'  => $from_name,
+                'from_email' => $from_email,
+
+                'subject' => $subject,
+
+                'text_body' => $text_body,
+                'html_body' => $html_body,
+            ]
+        );
     }
 
     /**
@@ -201,13 +185,6 @@ class RveSparkPost extends AbsBase
 
             'text_body' => '',
             'html_body' => '',
-
-            'spam_score' => 0.0,
-
-            'spf_result' => 'none',
-
-            'dkim_signed' => false,
-            'dkim_valid'  => false,
         ];
         $args = array_merge($default_args, $args);
         $args = array_intersect_key($args, $default_args);
@@ -222,15 +199,6 @@ class RveSparkPost extends AbsBase
         $text_body = trim((string) $args['text_body']);
         $html_body = trim((string) $args['html_body']);
 
-        $spam_score = (float) $args['spam_score'];
-
-        $spf_result = trim(strtolower((string) $args['spf_result']));
-
-        $dkim_signed = (bool) $args['dkim_signed'];
-        $dkim_valid  = (bool) $args['dkim_valid'];
-
-        $force_status = null; // Initialize.
-
         if (!$reply_to_email) { // Must have this.
             return; // Missing `Reply-To:` address.
         }
@@ -240,23 +208,6 @@ class RveSparkPost extends AbsBase
         if (!($rich_text_body = $this->coalesce($html_body, $text_body))) {
             return; // Empty reply; nothing to do here.
         }
-        if ($spam_score >= (float) $this->plugin->options['rve_mandrill_max_spam_score']) {
-            $force_status = 'spam'; // Force this to be considered `spam`.
-        }
-        if (($spf_check = (int) $this->plugin->options['rve_mandrill_spf_check_enable'])) {
-            if (($spf_check === 1 && !in_array($spf_result, ['pass', 'neutral', 'softfail', 'none'], true))
-                || ($spf_check === 2 && !in_array($spf_result, ['pass', 'neutral', 'none'], true))
-                || ($spf_check === 3 && !in_array($spf_result, ['pass', 'neutral'], true))
-                || ($spf_check === 4 && !in_array($spf_result, ['pass'], true))
-            ) {
-                $force_status = 'spam'; // Force this to be considered `spam`.
-            }
-        }
-        if (($dkim_check = (int) $this->plugin->options['rve_mandrill_dkim_check_enable'])) {
-            if (($dkim_check === 1 && $dkim_signed && !$dkim_valid) || ($dkim_check === 2 && (!$dkim_signed || !$dkim_valid))) {
-                $force_status = 'spam'; // Force this to be considered `spam`.
-            }
-        }
         $post_comment_args = compact(
             'reply_to_email',
             //
@@ -265,9 +216,7 @@ class RveSparkPost extends AbsBase
             //
             'subject',
             //
-            'rich_text_body',
-            //
-            'force_status'
+            'rich_text_body'
         );
         $this->plugin->utils_rve->maybePostComment($post_comment_args);
     }
